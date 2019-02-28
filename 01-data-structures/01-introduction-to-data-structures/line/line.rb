fa88f72f813ec049e02aedbe0ca040df7e5b7589
Stephanie <esceeaitchyu@gmail.com>
2019-02-28 08:05:57 -0800

    Completion of Checkpoint 01: Introduction to Data Structures

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,37 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    members.delete(person)
   end
 
   def front
+    members.first
   end
 
   def middle
+    members[((members.length)/2)]
   end
 
   def back
+    members.last
   end
 
   def search(person)
+    if members.include?(person)
+      return person
+    else
+      return nil
+    end
   end
 
   private
 
   def index(person)
+    members
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,4 +1,5 @@
 require_relative 'pixel'
+require 'matrix'
 
 class Screen
   attr_accessor :width
@@ -6,18 +7,28 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = Matrix.build(height, width) {|row, col| 0 }.to_a
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
+    inbounds(x, y)
   end
 
   private
 
   def inbounds(x, y)
+    if x < 0 || y < 0
+      return nil
+    else
+      @matrix[x][y]
+    end
   end
 
 end
