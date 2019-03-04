9b3363fee8e4fb5262b2b340e0326208cc94a626
Stephanie <esceeaitchyu@gmail.com>
2019-03-04 12:10:25 -0800

    Completion of Checkpoint 02: Stacks and Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,22 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = @queue[@queue.length-1]
   end
 
   def dequeue
+    @queue.delete_at(0)
+    @tail = @queue[@queue.length-1]
+    @head = @queue[0]
   end
 
   def empty?
+    if @queue.length == 0
+      return true
+    else
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,23 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack[@stack.length-1]
   end
 
   def pop
+    temp =  @stack[@stack.length-1]
+    @stack.delete_at(@stack.length-1)
+    self.top = @stack[@stack.length-2]
+    return temp
   end
 
   def empty?
+    if @stack.length == 0
+      return true
+    else
+      return false
+    end
   end
+
 end
