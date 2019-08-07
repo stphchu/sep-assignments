e2ee22cb6af80adefc920b4fdb4d107c008c017d
Stephanie <esceeaitchyu@gmail.com>
2019-08-06 21:40:54 -0700

    Completion of Checkpoint 04: Hashes - Part 1

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,56 @@ class HashClass
   end
 
   def []=(key, value)
+    index = self.index(key, self.size)
+    if @items[index] != nil
+      if @items[index].key != key
+        self.resize
+        self[key] = value
+      else
+        item = @items[index]
+        if item.value != value
+          self.resize
+        end
+        item.value = value
+      end
+    else
+      @items[index] = HashItem.new(key, value)
+    end
   end
 
-
   def [](key)
+    @items[self.index(key, self.size)].value if @items[self.index(key, self.size)]
   end
 
   def resize
+    old_items = @items
+    @items = Array.new(@items.size * 2)
+    old_items.each do |item|
+      if item != nil
+        index = self.index(item.key, self.size)
+        if @items[index] != nil
+          self[item.key] = item.value
+        else
+          @items[index] = item
+        end
+      end
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    sum = 0
+    key.each_byte do |c|
+      sum += c
+    end
+    sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
